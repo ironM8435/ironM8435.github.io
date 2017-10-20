c5261917e3adf1ebdfa7104a685eb40580e87434
Peyton Kirby Pierce <peytonkirby739841@gmail.com>
2017-10-20 11:20:35 +0000

    Create personal website

--- dawgshop/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('go').onclick = function() {
-    var inputText = $('#text-input').val();
-    var dawgifiedText = inputText + ", dawg";
-    document.getElementById('output').innerHTML = dawgifiedText;
-};
